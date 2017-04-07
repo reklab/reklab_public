9604a44f1a36fe3d6807912ba085c5f0d912e1f0
Robert Kearney <robert.kearney@mcgill.ca>
2017-04-07 09:40:44 -0400

    tvm: major redesign

--- nlid_tools/@tvm/tvm.m
+++ nlid_tools/@tvm/tvm.m
@@ -1,28 +1,245 @@
 classdef tvm < nlm
+    % Time Varyibg Model class
     % define and create time-varying model objects
     %  Parent: nldat
     
     % Copyright 2000, Robert E Kearney
     % This file is part of the nlid toolbox, and is released under the GNU
     % General Public License For details, see ../copying.txt and ../gpl.txt
+    
     properties
-            tvFlag=true
+        tvStart=nan;
+        tvIncr=0;
     end
     
     methods
         
-        function TVM=tvm(a,varargin);
-            % Add object  specific parameters
-            N.comment='IRF Model';
+        function TVM = tvm(a, varargin)
+            TVM.comment='TV Model';
             if nargin==0;
                 return
             elseif nargin==1,
-                N=nlmkobj(N,a);
-            elseif isa(a,'nlm')
-                N=nlmkobj(a,varargin{:});
+                TVM=nlmkobj(I,a);
+            elseif isa(a,'irf')
+                TVM=nlmkobj(a,varargin{:});
             else
-                N.nlmkobj(N,a,varargin{:});
+                TVM=nlmkobj(I,a,varargin{:});
             end
         end
+        
+        function plot (TVM)
+            el=TVM.elements;
+            nEl=length(el);
+            % Case for IRF models 
+            if isa(el{1},'irf'),
+                
+                for iEl=1:nEl,
+                    curEl=el{iEl};
+                    curVal=double(curEl);
+                    if iEl==1,
+                        Z=zeros(nEl,length(curVal));
+                    end
+                    Z(iEl,:)=curVal;
+                    
+                end
+                X=domain(TVM);
+                Y=domain(curEl);
+                mesh(Y,X,Z);
+                xlabel('Lag');
+                ylabel('Time');
+                title(TVM.comment); 
+                % Case for polynomial models
+            elseif isa(el{1},'polynom')
+                for iEl=1:nEl,
+                     curP=el{iEl};
+                     if iEl==1,
+                         pRange=curP.polyRange;
+                         xVal=linspace(pRange(1),pRange(2));
+                         Z=zeros(nEl,length(xVal)) ;
+                     end
+                     Z(iEl,:)=double(nlsim(curP,xVal'));   
+                end
+       
+                X=domain(TVM);
+                mesh(xVal,X,Z); 
+                xlabel('Input Value');
+                ylabel('Time');
+                zlabel('Output Value');
+                title(TVM.comment); 
+            elseif isa(el{1},'nlm')
+                firstEl=el{1};
+                [nRow,nCol]=size(firstEl);
+                figNum=0;
+                nFig=nRow*nCol;
+                for iRow=1:nRow,
+                    for iCol=1:nCol
+                        curBlock=block(TVM,iRow,iCol);
+                        figNum=figNum+1;
+                        subplot (nRow,nCol,figNum);
+                        modelType=class(curBlock.elements{1});
+                        set(curBlock,'comment', [ curBlock.comment ': ' modelType]); 
+                        plot(curBlock);
+                   end
+                end
+                
+            else
+                error('model type not supported');
+                
+            end
+            
+        end
+        function yPre = nlsim( TVM, xIn)
+            % Simulation the ensemble response of a TVM model
+            if TVM.tvIncr ~= xIn.domainIncr
+                error ('TV Model and input must have the same sampling rate');
+            end
+            indexStart=(TVM.tvStart-xIn.domainStart)/TVM.tvIncr;
+            index=indexStart;
+            dt=TVM.tvIncr;
+            yPre=xIn;
+            [m,n,p]=size(xIn);
+            yData=zeros(m,n,p);
+            el=TVM.elements;
+            nEl= length(TVM.elements);
+            for iEl=1:nEl,
+                curModel=el{iEl};
+                if isa(curModel,'irf'),
+                    yTemp=  tvIRFsim(curModel.dataSet,xIn.dataSet,index,curModel.nSides, dt);
+                    yData(index,1,:)=yTemp;
+                elseif isa(curModel,'polynom')
+                    yTemp=nlsim(curModel,xIn(index,1,:));
+                    yData(index,1,:)=double(yTemp);
+                elseif isa(curModel,'nlbl')
+                    pCur=curModel{1};
+                    xTemp=xIn;
+                    xTemp=nlsim(pCur,xIn);
+                    curIRF=curModel{2};
+                    yTemp= tvIRFsim(curIRF.dataSet,xTemp.dataSet,index,curIRF.nSides,dt );
+                    yData(index,1,:)=yTemp;
+                    
+                else
+                    error ('TV Model not supported');l
+                end
+                index=index+1;
+            end
+            set(yPre,'dataSet',yData);
+            
+            
+            
+            function y = tvIRFsim(H, X, i, nSides, dt)
+                % Simulate the response of a TV IRF
+                hlen = length(H);
+                if nSides == 2
+                    x = X(i-(hlen-1)/2:i+(hlen-1)/2,:);
+                else
+                    x = X(i-hlen+1:i,:);
+                end
+                y = (flipud(H)'*x*dt)';
+            end
+            
+            function y = tvNLBLsim(NLBL, X, i)
+                % Simulate the response of a TV IRF
+                p=NLBL{1};
+                hIRF=NLBL{2};
+                nSides=hIRF.nSides;
+                H=double(hIRF); H=H(:);
+                dt=X.domainIncr;
+                hlen = length(H);
+                if nSides == 2
+                    x = X(i-(hlen-1)/2:i+(hlen-1)/2,1,:);
+                else
+                    x = X(i-hlen+1:i,1,:);
+                end
+                xp=nlsim(p,x);
+                x=double(xp); x=squeeze(x);
+                y = (flipud(H)'*x*dt)';
+            end
+        end
+        
+        
+        function NN= normGainLE ( N )
+            % Normalize a TV nlblmodel  to assign all gain to the NL element.
+            % Scale Hammerstein model apropriately
+            % Only valid for low pass systems.
+            NN=N;
+            nElements=length(N.elements);
+            for iElement=1:nElements,
+                curElement=N.elements{iElement};
+                if ~strcmp(class(curElement),'nlbl'),
+                    error('Model must be of type nlbl');
+                end
+                normElement=normGainLE(curElement);
+                NN.elements{iElement}=normElement;
+                set(NN,'comment','Normalized nlbl');
+            end
+        end
+        
+        function tvBlock = block (TVM, row, col );
+            % Return block row,col from TVM
+            tvBlock=TVM;
+            el=TVM.elements;
+            nEl=length(TVM.elements);
+            subElements={};
+            for iEl=1:nEl,
+                curEl=el{iEl};
+                tempEl=curEl{row,col};
+                subElements{iEl}=tempEl;
+            end
+            tvBlock.elements=subElements;
+            
+        end
+        
+        function d = domain(TVM)
+            %Return domain values for a tvm
+            nEl=length(TVM.elements);
+            dt=TVM.tvIncr;
+            t=TVM.tvStart-dt;
+            for i=1:nEl
+                t=t+dt;
+                d(i)=t;
+            end
+        end
+        
+        function Value=subsref( sys, S )
+            %% {} returns a cell array with individual models for  specified indices
+            %% () returns a TVM with elements specified, as  ell array of more thn model
+            n=length(S);
+            Value=sys;
+            for iS=1:length(S),
+                switch S(iS).type
+                    case '.',
+                        Value = get(Value, S(iS).subs);
+                    case '{}'
+                        nDim=length(S(iS).subs);
+                        if nDim==1,
+                            Value={Value.elements{S(iS).subs{1}}};
+                            if length(Value)==1,
+                                Value=Value{1};
+                            end
+                        elseif nDim==2,
+                            Value=Value.elements{S(iS).subs{1},S(iS).subs{2}};
+                        else
+                            error ('Unexpected values');
+                        end
+                    case '()'
+                        nDim=length(S(iS).subs);
+                        if nDim==1,
+                            t=domain(Value);
+                            t=t(S(iS).subs{1});
+                            Value.tvStart=t(1);
+                            Value.elements={Value.elements{S(iS).subs{1}}};
+                        elseif nDim==2,
+                            Value.elements=Value.elements{S(iS).subs{1},S(iS).subs{2}};
+                        else
+                            error ('Unexpected values');
+                        end
+                    otherwise
+                        Value=builtin('subsref',Value,S(iS));
+                end
+            end
+        end
+        
+        
     end
 end
+
