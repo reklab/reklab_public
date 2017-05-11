f54db5142a3c8d08826163beab1e4d18589560f7
Robert Kearney <robert.kearney@mcgill.ca>
2017-05-11 14:14:05 -0400

    Added support for identification of TV IRFs using the ensmble method

--- nlid_tools/@tvm/tvm.m
+++ nlid_tools/@tvm/tvm.m
@@ -1,9 +1,9 @@
 classdef tvm < nlm
     % Time Varyibg Model class
     % define and create time-varying model objects
-    %  Parent: nldat
+    %  Parent: nlm
     
-    % Copyright 2000, Robert E Kearney
+    % Copyright 2017, Robert E Kearney
     % This file is part of the nlid toolbox, and is released under the GNU
     % General Public License For details, see ../copying.txt and ../gpl.txt
     
@@ -16,10 +16,16 @@ classdef tvm < nlm
         
         function TVM = tvm(a, varargin)
             TVM.comment='TV Model';
+            TVM.parameterSet(1)=param('paramName','tvIdentMethod','paramDefault','manual',...
+                'paramHelp', 'Method used to create tvm ', ...
+                'paramType','select',...
+                'paramLimits',{'manual','ensemble'});
             if nargin==0;
                 return
             elseif nargin==1,
                 TVM=nlmkobj(I,a);
+            elseif isa(a,'nldat'),
+                TVM = nlident(tvm, a, varargin{:});
             elseif isa(a,'irf')
                 TVM=nlmkobj(a,varargin{:});
             else
@@ -30,7 +36,7 @@ classdef tvm < nlm
         function plot (TVM)
             el=TVM.elements;
             nEl=length(el);
-            % Case for IRF models 
+            % Case for IRF models
             if isa(el{1},'irf'),
                 
                 for iEl=1:nEl,
@@ -47,25 +53,25 @@ classdef tvm < nlm
                 mesh(Y,X,Z);
                 xlabel('Lag');
                 ylabel('Time');
-                title(TVM.comment); 
+                title(TVM.comment);
                 % Case for polynomial models
             elseif isa(el{1},'polynom')
                 for iEl=1:nEl,
-                     curP=el{iEl};
-                     if iEl==1,
-                         pRange=curP.polyRange;
-                         xVal=linspace(pRange(1),pRange(2));
-                         Z=zeros(nEl,length(xVal)) ;
-                     end
-                     Z(iEl,:)=double(nlsim(curP,xVal'));   
+                    curP=el{iEl};
+                    if iEl==1,
+                        pRange=curP.polyRange;
+                        xVal=linspace(pRange(1),pRange(2));
+                        Z=zeros(nEl,length(xVal)) ;
+                    end
+                    Z(iEl,:)=double(nlsim(curP,xVal'));
                 end
-       
+                
                 X=domain(TVM);
-                mesh(xVal,X,Z); 
+                mesh(xVal,X,Z);
                 xlabel('Input Value');
                 ylabel('Time');
                 zlabel('Output Value');
-                title(TVM.comment); 
+                title(TVM.comment);
             elseif isa(el{1},'nlm')
                 firstEl=el{1};
                 [nRow,nCol]=size(firstEl);
@@ -77,9 +83,9 @@ classdef tvm < nlm
                         figNum=figNum+1;
                         subplot (nRow,nCol,figNum);
                         modelType=class(curBlock.elements{1});
-                        set(curBlock,'comment', [ curBlock.comment ': ' modelType]); 
+                        set(curBlock,'comment', [ curBlock.comment ': ' modelType]);
                         plot(curBlock);
-                   end
+                    end
                 end
                 
             else
@@ -88,12 +94,63 @@ classdef tvm < nlm
             end
             
         end
+        
+        function TVM = nlident ( TVM, Z, varargin)
+            disp(Z);
+            modelPrototype=varargin{1};
+            modelType =class(varargin{1});
+            switch modelType
+                case 'irf'
+                    disp('estimate a TV irf');
+                    X=squeeze(double(Z(:,1,:)));
+                    Y=squeeze(double(Z(:,2,:)));
+                    dt=Z.domainIncr;
+                    nSides=modelPrototype.nSides;
+                    nLags=modelPrototype.nLags;
+                    approach=modelPrototype.irfIdMethod;
+                    conf_level=modelPrototype.irfErrorLevel;
+                    if strcmp(approach,'pseudo'),
+                        [hIdent,bound,sing_vectors,cpu] = tvIdentEnsemble(X,Y,dt,nSides,nLags,approach,conf_level);
+                    else
+                        [hIdent] = tvIdentEnsemble(X,Y,dt,nSides,nLags,approach);
+                    end
+                    if nSides ==2,
+                        LagStart=-nLags*dt;
+                        TimeStart=nLags*dt;
+                    else
+                        LagStart=0;
+                        TimeStart=(nLags-1)*dt;
+                    end
+                    i=modelPrototype;
+                    set(i,'nSides',nSides, 'domainIncr',dt, ...
+                        'nLags',nLags,'domainStart', LagStart, 'domainName','Lag');
+                    
+                    [nReal,nLag]=size(hIdent);
+                    I={};
+                    for iReal=1:nReal,
+                        curIRF=hIdent(iReal,:);
+                        set(i,'dataSet', curIRF);
+                        I{iReal}=i;
+                    end
+                    set(TVM,'tvStart',TimeStart,'tvIncr',dt,'elements',I');
+                    
+                otherwise
+                    disp(['tvm: identification not supported for model  type: ' modelType ]);
+            end
+            %% Reformat for output.
+            
+            
+            disp(modelType);
+        end
+        
+        
+        
         function yPre = nlsim( TVM, xIn)
-            % Simulation the ensemble response of a TVM model
+            % Simulate the ensemble response of a TVM model
             if TVM.tvIncr ~= xIn.domainIncr
                 error ('TV Model and input must have the same sampling rate');
             end
-            indexStart=(TVM.tvStart-xIn.domainStart)/TVM.tvIncr;
+            indexStart=1 +(TVM.tvStart-xIn.domainStart)/TVM.tvIncr;
             index=indexStart;
             dt=TVM.tvIncr;
             yPre=xIn;
@@ -128,17 +185,24 @@ classdef tvm < nlm
             
             function y = tvIRFsim(H, X, i, nSides, dt)
                 % Simulate the response of a TV IRF
+                [n,n,r]=size(X);
                 hlen = length(H);
-                if nSides == 2
-                    x = X(i-(hlen-1)/2:i+(hlen-1)/2,:);
-                else
-                    x = X(i-hlen+1:i,:);
+                z=zeros(hlen,1);
+                i=i+hlen;
+                for iReal=1:r
+                    Xpad=cat(1,z,X(:,1,iReal),z);
+                    
+                    if nSides == 2
+                        x = Xpad(i-(hlen-1)/2:i+(hlen-1)/2,:);
+                    else
+                        x = Xpad(i-hlen+1:i,:);
+                    end
+                    y(iReal) = (flipud(H(:))'*x*dt)';
                 end
-                y = (flipud(H)'*x*dt)';
             end
             
             function y = tvNLBLsim(NLBL, X, i)
-                % Simulate the response of a TV IRF
+                % Simulate the response of a TV NLBL
                 p=NLBL{1};
                 hIRF=NLBL{2};
                 nSides=hIRF.nSides;
