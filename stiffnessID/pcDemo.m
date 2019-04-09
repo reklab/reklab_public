1d97ef67749553d62c2983f3e6c18195fab4f4ba
Robert Kearney <robert.kearney@mcgill.ca>
2019-04-09 15:50:08 -0400

    1) Changes to accout for changes in parameter names in nlbl. (2) Make plotting a subroutine. (3) Plot all methods in different windows.

--- stiffnessID/pcDemo.m
+++ stiffnessID/pcDemo.m
@@ -1,12 +1,46 @@
-%% pcDemo
+function pcDemo
+%% Demonstrate various aproaches to the identificatio of the parallel cascad emode 
 load experimental_data.mat
 z=z_pf2;
-[hIntrinsic, mReflex, nlmStiff, VAFT, VAFi, VAFr, tqI, tqR, tqT]= PC_stiffnessID (z);
+%% Rectifier followed by IRF 
+reflexIdMethod='rect'; 
+[hIntrinsic, mReflex, nlmStiff, VAFT, VAFi, VAFr, tqI, tqR, tqT]= PC_stiffnessID (z,'reflex_id_method',reflexIdMethod);
+figNum=1;
+pcDemoPlot (z, tqI, tqR, tqT, nlmStiff, figNum)
+
+
+%%  Estimate reflex pathway using HK method 
+reflexIdMethod='hk'; 
+[hIntrinsic, mReflex, nlmStiff, VAFT, VAFi, VAFr, tqI, tqR, tqT]= PC_stiffnessID (z,'reflex_id_method',reflexIdMethod);
+figNum=3;
+pcDemoPlot (z, tqI, tqR, tqT, nlmStiff, figNum)
+
+%%  Estimate reflex pathway using sls method 
+reflexIdMethod=''; 
+[hIntrinsic, mReflex, nlmStiff, VAFT, VAFi, VAFr, tqI, tqR, tqT]= PC_stiffnessID (z,'reflex_id_method',reflexIdMethod);
+figNum=5;
+pcDemoPlot (z, tqI, tqR, tqT, nlmStiff, figNum)
+
+
 %% Yong's state-space method
-[system_ss, pIntrinsic, tqPredicted]= SS_stiffnessID (z);
+%%[system_ss, pIntrinsic, tqPredicted]= SS_stiffnessID (z);
+
+
 %% SDSS state-space method
+figNum=7;
+
 [intrinsic, reflex, tqI, tqR, tqT] = SDSS_stiffnessID (z);
+nlmStiff=nlm;
+nlmStiff{1,1}=intrinsic;
+nlmStiff{2,1}=reflex; 
+figNum=5;
+pcDemoPlot (z, tqI, tqR, tqT, nlmStiff, figNum)
+
+
+
 %%
+end
+function pcDemoPlot (z, tqI, tqR, tqT, nlmStiff, figNum)
 figNum=1;
 xLim=[10 20];
 pos=decimate(z(:,1),1);
@@ -16,62 +50,47 @@ vafR=vaf(tq-tqI,tqR);
 vafT=vaf(tq,tqT)
 
 figure(figNum);
-subplot (4,1,1);
-plot(pos);set(gca,'xlim',xLim),set(gca,'xlim',xLim,'xticklabel',{}); 
-; 
-title('Position');
-xlabel('');
-ylabel('rad');
+subplot (2,2,1);
+plot (nlmStiff{1,1}); 
+subplot (2,2,3);
+R=(nlmStiff{2,1})
+plot(R{1,1});
+subplot (2,2,4);
+plot (R{1,2})
+streamer (nlmStiff.comment); 
 
 
-subplot (4,1,2);
-plot(tq,'line_color','r');
-set(gca,'xlim',xLim,'xticklabel',{}); 
-title('Torque');
-xlabel('');
-ylabel('Nm');
-set(gca,'xlim',xLim,'xticklabel',{}); 
-
-subplot (4,1,3);
-title('GS EMG');
-xlabel('');
-set(gca,'xlim',xLim,'xticklabel',{}); 
-
-
-set(gca,'xlim',xLim); 
-subplot (4,1,4);
-title('TA EMG')
-xlabel('');
-
-set(gca,'xlim',xLim); 
-fig_mod(figNum);
-
+figMod(figNum);
+%%
 figNum=figNum+1;
 figure(figNum);  clf
 subplot (4,1,1);
-plot(pos); set(gca,'xlim',xLim, 'xticklabel',{}); 
+plot(pos); set(gca,'xlim',xLim, 'xticklabel',{});
 title('Position');
 xlabel('');
 ylabel('rad');
-subplot (4,1,2); plot(tqI,'line_color','m');
-set(gca,'xlim',xLim,'xticklabel',{}); 
+subplot (4,1,2); plot(tqI,'linecolor','m');
+set(gca,'xlim',xLim,'xticklabel',{});
 title('Intrinsic Torque')
 ylabel('Nm')
 xlabel('');
-subplot  (4,1,3); plot (tqR, 'line_color','g');
-set(gca,'xlim',xLim, 'xticklabel',{}); 
+subplot  (4,1,3); plot (tqR, 'linecolor','g');
+set(gca,'xlim',xLim, 'xticklabel',{});
 title('Reflex Torque')
 ylabel('Nm');
 xlabel('');
 subplot (4,1,4);
-plot(tq,'line_color','r');
+plot(tq,'linecolor','r');
 h=line(tqT+mean(tq));
 set(h,'color','cyan')
-set(gca,'xlim',xLim); 
-title('Total Torque');
+set(gca,'xlim',xLim);
+title(['Total Torque %VAF=' num2str(vafT.dataSet)]);
 ylabel('Nm');
 legend('Observed','Predicted','Location','Southwest');
-fig_mod(figNum);
+streamer(nlmStiff.comment); 
+figMod(figNum);
+end
+
 
 
 
