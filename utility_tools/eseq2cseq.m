077e2fe7c45d8311b7f403cd23189c8311d34f56
h664501 <robert.kearney@mcgill.ca>
2021-11-03 10:43:49 -0400

    Deleted file eseq2eseq since its functionality is replaced by eseq.cseq

--- utility_tools/eseq2cseq.m
+++
@@ -1,9 +0,0 @@
-function cseq = eseq2cseq (eseq)
-% eseq2cseq - converts an event sequent to to a categorical sequence
-
-cseq=categorical;
-for i=1:length(eseq)
-  cseq(eseq(i).start:eseq(i).end,1)=eseq(i).type; 
-end
-
-
